36b5ea24a992fc9e4fcace96392b3968e7ba0bea
Meek Msaki <meek10x@gmail.com>
2026-01-25 18:42:45 -0600

    add buildernet example

---
+++ packages/buildernet-rpc/example.ts
@@ -0,0 +1,19 @@
+import { BuildernetClient } from "./src";
+
+const rpc = "https://rpc.buildernet.org";
+const client = new BuildernetClient(rpc);
+const bundle = {
+  txs: ["0x123abc", "0x456def..."] as Hex[],
+  blockNumber: "0xb63dcd" as Hex,
+  minTimestamp: 0,
+  maxTimestamp: 1615920932,
+};
+const body = client.rpc.buildRequest("eth_sendBundle", [bundle]);
+// const signature = wallet.sign(body)
+// const sender = wallet.address
+const result = await client
+  .setHeaders({
+    "X-Flashbots-Signature": `0x<sender>:0x<signature>`,
+  })
+  .eth_sendBundle(bundle);
+console.log(result);
